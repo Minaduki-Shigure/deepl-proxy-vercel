1749792ff703ee1783db963e2648341831f12bd9
Minaduki Shigure <wukangzheng991025@gmail.com>
2023-10-07 19:11:41 +0800

    Update and rename index.ts to [...].ts

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,6 +1,6 @@
 export default defineEventHandler((event) => {
   if (getRequestURL(event)) {
-    return { hello: 'world' };
+    const { pathname, search } = new URL(event.request.url);
     var headers = event.request.headers;
     var deepl_keys = process.env.DEEPL_KEYS.split(",");
     var expected_key = process.env.PSEUDO_DEEPL_KEY;
@@ -9,6 +9,8 @@ export default defineEventHandler((event) => {
       var rand = Math.floor(Math.random() * deepl_keys.length);
       var deepl_key = deepl_keys[rand];
       event.request.headers.append("Authorization", `DeepL-Auth-Key ${deepl_key}`);
+      return new Response(`https://api-free.deepl.com${pathname}${search}`);
+      //return fetch(new Request(`https://api-free.deepl.com${pathname}${search}`, request));
     }
   }
 })
